d862c21202c6ccbffaa5901153aaf8dad827a96c
Zhuzhanchini <asot2006@gmail.com>
2021-12-08 19:50:41 +0300

    practice with fragments

---
+++ app/src/main/java/example/julia/fragments/EndFragment.kt
@@ -0,0 +1,21 @@
+package example.julia.fragments
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EndFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_end, container, false)
+
+        return view
+
+
+    }
+}
--- app/src/main/java/example/julia/fragments/MainActivity.kt
+++ app/src/main/java/example/julia/fragments/MainActivity.kt
@@ -2,10 +2,42 @@ package example.julia.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var switchFragmentButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        switchFragmentButton = findViewById(R.id.switch_fragments_button)
+
+
+        val startFragment = StartFragment()
+        val endFragment = EndFragment()
+
+        switchFragmentButton.setOnClickListener {
+            val fragment =
+                when (supportFragmentManager.findFragmentById(R.id.fragment_container)) {
+                    is StartFragment -> endFragment
+                    is EndFragment -> startFragment
+                    else -> startFragment
+                }
+
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+        }
+
+
+
     }
+
+
+
+
 }
+
