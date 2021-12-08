07a34b1daa5d0dbb3ebb1a552376a100e874d4e7
Zhuzhanchini <asot2006@gmail.com>
2021-12-08 20:58:39 +0300

    practice with fragments

--- app/src/main/java/example/julia/fragments/EndFragment.kt
+++ app/src/main/java/example/julia/fragments/EndFragment.kt
@@ -1,21 +1,21 @@
 package example.julia.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+
 class EndFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_end, container, false)
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_end2, container, false)
+    }
 
 
     }
-}
